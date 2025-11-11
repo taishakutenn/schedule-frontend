761d08dbc702aea85a1f3e8b16355e6c5a49f210
Cy_Nec <cy.nec01@gmail.com>
2025-11-11 20:03:49 +0500

    start plan page

--- src/pages/Plans.jsx
+++ src/pages/Plans.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import InfoBlock from "../components/infoBlock/InfoBlock";
 import Button from "../components/Button/Button";
 
@@ -13,14 +14,44 @@ const plansInfo = [
   },
 ];
 
+const planLoadHeaderInfo = [
+  {
+    title: "Загрузка учебных планов",
+    text: [],
+  },
+];
+
 export default function Plans() {
+  const [activeView, setActiveView] = useState(null);
+
+  const handleLoadPlan = () => setActiveView("loadPlan");
+  const handleTeachLoad = () => setActiveView("teachLoad");
+
+  const Content = () => {
+    if (activeView === "loadPlan") {
+      return (
+        <div className="dynamic-content">
+          <InfoBlock items={planLoadHeaderInfo} />
+          <div className="instruction"></div>
+        </div>
+      );
+    }
+
+    if (activeView === "teachLoad") {
+      return <p>TEACH LOAD</p>;
+    }
+
+    return null;
+  };
+
   return (
     <main>
       <InfoBlock items={plansInfo} />
-      <div class="btn--box">
-        <Button>Загрузка плана</Button>
-        <Button>Назначение учебной нагрузки</Button>
+      <div className="btn--box">
+        <Button onClick={handleLoadPlan}>Загрузка плана</Button>
+        <Button onClick={handleTeachLoad}>Назначение учебной нагрузки</Button>
       </div>
+      <div>{Content()}</div>
     </main>
   );
 }
