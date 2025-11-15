879f65636f8492f4b2d16f3e66849af7f53563a1
Cy_Nec <cy.nec01@gmail.com>
2025-11-15 18:18:08 +0500

    start creating add form for teacher

--- src/pages/Handbooks.jsx
+++ src/pages/Handbooks.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import HandbookTable from "../components/HandbookTable/HandbookTable";
 import { useApiData } from "../hooks/useApiData";
 import { tableConfig } from "../utils/tableConfig";
+import Modal from "../components/Modal/Modal";
+import { getTeachersCategory } from "../api/teacherCategoryAPI";
 
 import "./Handbook.css";
 
@@ -25,6 +27,16 @@ export default function Handbooks() {
     [handbook], // request when table changed
     !!currentTableConfig
   );
+
+  // state for display Modal window
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // load teacher category
+  const {
+    data: teachersCategoryData,
+    loading: teachersCategoryLoading,
+    error: teachersCategoryError,
+  } = useApiData(getTeachersCategory, [], isModalOpen);
 
   // set handbook states
   const handleTeacher = () => setHandbook("teachers");
@@ -70,29 +82,101 @@ export default function Handbooks() {
     return <HandbookTable apiResponse={filteredData} tableName={handbook} />;
   };
 
-  return (
-    <main>
-      <InfoBlock items={headerInfo} />{" "}
-      <div className="handbooks__navigation">
-        <Button onClick={handleTeacher}>Преподаватели</Button>
-        <Button onClick={handleTeacherCategory}>
-          Категории преподавателей
+  // Function to display modal window for adding
+  const ModalAdd = () => {
+    if (isModalOpen === false) return null;
+    if (handbook === "teachers") {
+      return (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          title="Добавить преподавателя"
+          size="sm"
+        >
+          <div className="teacher-add-container">
+            <input type="text" placeholder="Имя" />
+            <input type="text" placeholder="Фамилия" />
+            <input type="text" placeholder="Отчество" />
+            <input type="tel" placeholder="Номер телефона" />
+            <input type="email" placeholder="Почта" />
+            <input type="text" placeholder="Ставка" />
+
+            {teachersCategoryLoading && <div>Загрузка категорий...</div>}
+            {teachersCategoryError && <div>Ошибка загрузки категорий</div>}
+
+            {!teachersCategoryLoading && !teachersCategoryError && (
+              <select>
+                <option value="" disabled>
+                  Категория
+                </option>
+                {teachersCategoryData?.map((t) => (
+                  <option key={t.teacher_category} value={t.teacher_category}>
+                    {t.teacher_category}
+                  </option>
+                ))}
+              </select>
+            )}
+
+            <Button size="small">Добавить</Button>
+          </div>
+        </Modal>
+      );
+    }
+    return null;
+  };
+
+  // Function to display control container by condition
+  const ControlContainer = () => {
+    if (!handbook) {
+      return null;
+    }
+    return (
+      <div className="control-container">
+        <Button onClick={() => setIsModalOpen(true)} size="small">
+          Добавить запись
         </Button>
-        <Button onClick={handleSpeciality}>Специальности</Button>
-        <Button onClick={handleBuilding}>Здания</Button>
-        <Button onClick={handleCabinet}>Кабинеты</Button>
-        <Button onClick={handleSessionType}>Типы занятий</Button>
-      </div>
-      <div className="search-container">
+        <Button variant="secondary" size="small">
+          Редактировать запись
+        </Button>
+        <Button variant="danger" size="small">
+          Удалить запись
+        </Button>
         <input
           type="text"
           placeholder="Поиск"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <Button onClick={() => setSearch("")}>Очистить</Button>
       </div>
+    );
+  };
+
+  return (
+    <main>
+      <InfoBlock items={headerInfo} />{" "}
+      <div className="handbooks__navigation">
+        <Button onClick={handleTeacher} size="small">
+          Преподаватели
+        </Button>
+        <Button onClick={handleTeacherCategory} size="small">
+          Категории преподавателей
+        </Button>
+        <Button onClick={handleSpeciality} size="small">
+          Специальности
+        </Button>
+        <Button onClick={handleBuilding} size="small">
+          Здания
+        </Button>
+        <Button onClick={handleCabinet} size="small">
+          Кабинеты
+        </Button>
+        <Button onClick={handleSessionType} size="small">
+          Типы занятий
+        </Button>
+      </div>
+      <ControlContainer />
       <div className="rendered-table">{renderContent()}</div>
+      <ModalAdd />
     </main>
   );
 }
