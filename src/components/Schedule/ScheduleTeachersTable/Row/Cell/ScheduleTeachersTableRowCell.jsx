ed5104f1e057d1bab0795c393af16308a51d310b
taisha <taishakutennn@gmail.com>
2025-11-28 12:58:41 +0500

    add to table cell groups and subjects

--- src/components/Schedule/ScheduleTeachersTable/Row/Cell/ScheduleTeachersTableRowCell.jsx
+++ src/components/Schedule/ScheduleTeachersTable/Row/Cell/ScheduleTeachersTableRowCell.jsx
@@ -1,22 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import TeacherContext from "../../../../../contexts/TeacherContext";
 import { useApiData } from "../../../../../hooks/useApiData";
-import { getTeacherInPlanByTeacher } from "../../../../../api/teachersInPlansAPI"; 
 
 export default function ScheduleTeachersTableCell({classCell}) {
+  const [currentGroup, setCurrentGroup] = useState([]);
   // Get teacher info for context
   const teacher = useContext(TeacherContext);
+  // Get groups and subjects from context
+  const teacherGroups = teacher.teacherGroups;
+  const teacherSubjects = teacher.teacherSubjects;
 
   return (
       <td className={classCell}>
+
         <select className="teachers-table__select">
-          <option selected disabled></option>
-          <option value="23ИСП1">23ИСП1</option>
+          <option selected disabled>Выберите группу</option>
+          {teacherGroups.map((group) => {
+            return <option
+                    value={group}
+                    key={group}>
+                    {group}
+                    </option>
+          })}
         </select>
+
         <select>
-          <option selected disabled></option>
-          <option value="Математика">Математика</option>
+          <option selected disabled>Выберите предмет</option>
+          {teacherSubjects?.map((subject) => (
+            <option
+              value={subject.id}
+              key={subject.id}
+            >
+              {subject.title}
+            </option>
+          ))}
         </select>
       </td>
   );
--- src/components/Schedule/ScheduleTeachersTable/Row/ScheduleTeachersTableRow.jsx
+++ src/components/Schedule/ScheduleTeachersTable/Row/ScheduleTeachersTableRow.jsx
@@ -2,22 +2,66 @@ import ScheduleTeachersTableRowStudyDay from "./StudyDay/ScheduleTeachersTableRo
 import TeacherContext from "../../../../contexts/TeacherContext";
 import { useApiData } from "../../../../hooks/useApiData";
 import { getTeacherInPlanByTeacher } from "../../../../api/teachersInPlansAPI";
+import { getSubjectHoursByIds } from "../../../../api/subjectHoursAPI";
+import { getSubjectsByIds } from "../../../../api/subjectAPI";
+import { useState, useEffect } from "react";
 
 export default function ScheduleTeachersTableRow({
   selectedDate,
   teacherInfo,
 }) {
-  // Get groups for this teacher
-    const {
-      data: teacherInPlanData,
-      loading: teachersLoading,
-      error: teachersError,
-    } = useApiData(
-      () => getTeacherInPlanByTeacher(teacherInfo?.id),
-      [selectedDate]);
-  
-    console.log(teacherInPlanData);
+  // FIXED USE_API_DATA AND REWRITE THIS CODE
+  // =================================================================== 
+  // Get teacher in plan datas for current teacher
+  const [teachersInPlanData, setTeacherInPlanData] = useState([]);
+  const [teacherSubjects, setSubjects] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // First request
+        const result = await getTeacherInPlanByTeacher(teacherInfo?.id);
+        setTeacherInPlanData(result);
+        
+        // Second request
 
+        // Get subject_in_cycle_hours_ids
+        const subjectHoursIds = result.map(plan => plan.subject_in_cycle_hours_id);
+        // If we have subjectsHoursIds get subjectHours data
+        if (subjectHoursIds.length > 0) {
+          const subjectsHoursData = await getSubjectHoursByIds(subjectHoursIds);
+
+          // Get subject ids
+          const subjectIds = subjectsHoursData.map((subjectHour) => subjectHour.subject_in_cycle_id)
+          // If we have subjectIds get subjects data
+          if (subjectIds.length > 0) {
+            const subjects = await getSubjectsByIds(subjectIds);
+            setSubjects(subjects);
+          }
+        }
+        
+      } catch (err) {
+        setTeacherInPlanData([]);
+        console.error("Ошибка загрузки:", err);
+      } 
+    };
+
+    fetchData();
+  }, [selectedDate]);
+  // ===================================================================
+
+  // Get groups for current teacher
+  const teacherGroups = teachersInPlanData.map((plan) => plan.group_name);
+  // Get subjects in cycle hours id for current teacher
+  const teacherSubjectsInCycleHours = teachersInPlanData.map((plan) => plan.subject_in_cycle_hours_id);
+
+  // Create context
+  const teacherContext = {
+    "teacherInfo": teacherInfo,
+    "teacherGroups": teacherGroups,
+    "teacherSubjectsInCycleHours": teacherSubjectsInCycleHours,
+    "teacherSubjects": teacherSubjects
+  };
+  
   return (
     <tr>
       <td className="session-cell">
@@ -25,7 +69,7 @@ export default function ScheduleTeachersTableRow({
           ? `${teacherInfo.surname} ${teacherInfo.name.charAt(0)}. ${teacherInfo.fathername.charAt(0)}.`
           : `${teacherInfo.surname} ${teacherInfo.name.charAt(0)}.`}
       </td>
-      <TeacherContext.Provider value={teacherInfo}>
+      <TeacherContext.Provider value={teacherContext}>
         <ScheduleTeachersTableRowStudyDay />
         <ScheduleTeachersTableRowStudyDay />
         <ScheduleTeachersTableRowStudyDay />
