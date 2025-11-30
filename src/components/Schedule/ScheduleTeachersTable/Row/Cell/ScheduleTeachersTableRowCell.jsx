11f88d45f59666846ac6ba519b69cd3dfd6f0b4b
taisha <taishakutennn@gmail.com>
2025-11-30 10:27:25 +0500

    change to schedule teachers table

--- src/components/Schedule/ScheduleTeachersTable/Row/Cell/ScheduleTeachersTableRowCell.jsx
+++ src/components/Schedule/ScheduleTeachersTable/Row/Cell/ScheduleTeachersTableRowCell.jsx
@@ -13,29 +13,43 @@ export default function ScheduleTeachersTableCell({classCell}) {
 
   return (
       <td className={classCell}>
+        <div className="teachers-table__select-container">
+          <div className="teachers-table__group-subject-container">
+            <select className="teachers-table__select">
+              <option selected disabled className="select__option--disabled">Группа</option>
+              {teacherGroups.map((group) => {
+                return <option
+                        value={group}
+                        key={group}
+                        className="select__option"
+                        >
+                        {group}
+                        </option>
+              })}
+            </select>
 
-        <select className="teachers-table__select">
-          <option selected disabled>Выберите группу</option>
-          {teacherGroups.map((group) => {
-            return <option
-                    value={group}
-                    key={group}>
-                    {group}
-                    </option>
-          })}
-        </select>
-
-        <select>
-          <option selected disabled>Выберите предмет</option>
-          {teacherSubjects?.map((subject) => (
-            <option
-              value={subject.id}
-              key={subject.id}
-            >
-              {subject.title}
-            </option>
-          ))}
-        </select>
+            <select className="teachers-table__select">
+              <option selected disabled className="select__option--disabled">Предмет</option>
+              {teacherSubjects?.map((subject) => (
+                <option
+                  value={subject.id}
+                  key={subject.id}
+                  className="select__option"
+                >
+                  {subject.title}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="teachers-table__session-type-container">
+            <select teachers-table__select>
+              <option selected disabled className="select__option--disabled">Пара</option>
+              <option className="select__option">Лк</option>
+              <option className="select__option">Лб</option>
+              <option className="select__option">Пз</option>
+            </select>
+          </div>
+        </div>
       </td>
   );
 }
